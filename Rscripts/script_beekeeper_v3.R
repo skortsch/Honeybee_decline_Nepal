e9728b7cb42ee5b9a8196a7188d5b3fa7dbe3ef8
Kortsch <susakort@ad.helsinki.fi>
2023-03-30 14:31:41 +0300

    new figs

--- Rscripts/script_beekeeper_v3.R
+++ Rscripts/script_beekeeper_v3.R
@@ -341,17 +341,19 @@ pl.sel<-pd %>% filter(grepl('herb_spice|fruit_nut|oilseed|pulse', plant_category
 pl.sel$sci_name
 
 pl.sel2<-pd %>% filter(grepl('fruit_nut|oilseed|pulse', plant_category))
-pl.sel2$sci_name
+unique(pd$sci_name)
 pl.sel2$eng_name
 
-fruit_nut_oilseed_pulse_eng<-"Jumli bean|Apple|Soybean|Apricot|Plum|Mustard seed|Peach|Pear|Sunflower|Green bean|Scarlet bean|Cowpea|Almonds|Horse gram|Faba bean"
-fruit_nut_oilseed_pulse_sci<-"Phaseolus_vulgaris|Malus_domestica|Glycine_max|Prunus_armeniaca|Brassica_alba|Prunus_persica|Pyrus_communis|Helianthus_annuus|Phaseolus_vulgaris|Phaseolus_coccineus|Vigna_unguiculata|Amygdalus_communis|Macrotyloma_uniflorum|Vicia_faba"
+#fruit_nut_oilseed_pulse_eng<-"Jumli bean|Apple|Soybean|Apricot|Plum|Mustard seed|Peach|Pear|Sunflower|Green bean|Scarlet bean|Cowpea|Almonds|Horse gram|Faba bean"
+#fruit_nut_oilseed_pulse_sci<-"Phaseolus_vulgaris|Malus_domestica|Glycine_max|Prunus_armeniaca|Brassica_alba|Prunus_persica|Pyrus_communis|Helianthus_annuus|Phaseolus_vulgaris|Phaseolus_coccineus|Vigna_unguiculata|Amygdalus_communis|Macrotyloma_uniflorum|Vicia_faba"
+
 #all plants
-#hi_crop_fil<-hi_crop %>% filter(grepl("Phaseolus_vulgaris|Capsicum_sp.|Malus_domestica|Glycine_max|Prunus_armeniaca|Prunus_domestica|Brassica_alba|Prunus_persica|Pyrus_communis|Helianthus_annuus|Phaseolus_vulgaris|Phaseolus_coccineus|Vigna_unguiculata|Amygdalus_communis|Macrotyloma_uniflorum|Vicia_faba", Crop))
+hi_crop_fil2<-hi_crop %>% filter(grepl("Phaseolus_vulgaris|Capsicum_sp.|Cucurbita_maxima|Malus_domestica|Glycine_max|Cucumis_sativus|Cyclanthera_pedata|Prunus_armeniaca|Solanum_lycopersicon|Fagopyrum tataricum|Brassica_alba|Solanum melongena|Prunus_persica|Pyrus_communis|Helianthus_annuus|Phaseolus_vulgaris|Phaseolus_coccineus|Cyphomandra_betacea|Vigna_unguiculata|Momordica_charantia|Amygdalus_communis|Macrotyloma_uniflorum|Vicia_faba", Crop))
+#|Prunus_domestica #prunus domestica removed because it only contains zeros
 #Jumli bean|Apple|Soybean|Apricot|Plum|Mustard seed|Peach|Pear|Sunflower|Green bean|Scarlet bean|Cowpea|Almonds|Horse gram|Faba bean
 
-hi_crop_fil2<-hi_crop %>% filter(grepl("Malus_domestica|Prunus_armeniaca|Brassica_alba|Prunus_persica|Pyrus_communis|Helianthus_annuus|Amygdalus_communis", Crop))
-hi_crop_fil2<-hi_crop %>% filter(grepl(fruit_nut_oilseed_pulse_sci, Crop))
+#hi_crop_fil2<-hi_crop %>% filter(grepl("Malus_domestica|Prunus_armeniaca|Brassica_alba|Prunus_persica|Pyrus_communis|Helianthus_annuus|Amygdalus_communis", Crop))
+#hi_crop_fil2<-hi_crop %>% filter(grepl(fruit_nut_oilseed_pulse_sci, Crop))
 
 #"Phaseolus_vulgaris|Capsicum_sp.|Malus_domestica|Glycine_max|Prunus_armeniaca|Prunus_domestica|Brassica_alba|         
 #  Prunus_persica|Pyrus_communis|Helianthus_annuus|Phaseolus_vulgaris|Phaseolus_coccineus|Vigna_unguiculata|
@@ -366,18 +368,31 @@ hi_crop_fil2$n_nas <- ifelse(hi_crop_fil2$vQ==0, NA, hi_crop_fil2$vQ)
 hi_crop_fil3<-hi_crop_fil2 %>% complete(Crop, Village)
 hi_crop_fil3$n_nas <- ifelse(hi_crop_fil3$vQ==0, NA, hi_crop_fil3$vQ)
 
+colnames(hi_crop_fil3)[7]<-"% visits"
+  
 ggplot(hi_crop_fil3, aes(x=Village, y=Crop, fill=n_nas)) +
   geom_tile(color="white", size = 0.5) +
   geom_text(aes(label = round(n_nas,  digits=2)), angle = 0, size=3) + 
   scale_fill_gradient(low="gold", high="darkorchid", na.value="white", name = "% visits")+
   theme(axis.text.x = element_text(color = "black", size = 12, angle = 90))
 
+ggballoonplot(hi_crop_fil3, x = "Village", y = "Crop", size = "% visits",
+              fill = "% visits",
+              size.range = c(1, 13),
+              ggtheme = theme_bw(),
+              font.label = list(style="italic", size = 14, color = "black")) +
+              scale_fill_viridis_c(option = "C", name = "% visits")
+              
+hi_crop_fil3 %>% group_by(Crop) %>% 
+  summarise(meanQ=mean(vQ, na.rm = TRUE), max=max(vQ, na.rm = TRUE)) %>% 
+  mutate(rank.mean = dense_rank(desc(meanQ))) %>% 
+  arrange(rank.mean, desc(rank.mean)) 
+
+#Villages observed  
+hi_crop_fil2 %>% group_by(Crop) %>% summarise(Village=n())
+
 ggsave(paste0(dirF, "heatmap_prop_vis_Apis.png"),width=8, height = 6, units="in", dpi=600 ) 
 
-ggplot(new_dat, aes(x=Importance, y=Crop)) +
-  facet_wrap(~Village)+
-  geom_bar(stat="identity", width=.5, fill="tomato3") + 
-  theme(axis.text.x = element_text(angle=65, vjust=0.6))
 ############################################
 
 
