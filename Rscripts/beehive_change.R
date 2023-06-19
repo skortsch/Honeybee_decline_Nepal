2a4c29bf5ee555424bf78121140949cbe27a3f95
Kortsch <susakort@ad.helsinki.fi>
2023-06-19 14:00:08 +0300

    updated files

--- Rscripts/beehive_change.R
+++ Rscripts/beehive_change.R
@@ -17,7 +17,95 @@ bc
 head(bc)
 colnames(bc)
 
+#BEEHIVE DECLINE
 bh_t <- pivot_longer(bc[,],cols = c("2009", "2012", "2017","2019","2021", "2022"), names_to = "year")
+
+#boxplots exploratory
+ggplot(bh_t) + geom_boxplot(aes(year, value), na.rm = FALSE)
+#ggplot(bh_t, aes(value)) + geom_histogram(binwidth = 0.2)
+#ggplot(bh_t, aes(value)) + facet_wrap(~year, scales = 'free_x') + geom_histogram(binwidth = 0.2)
+
+mod.lm<-lm(log(value+1) ~ as.numeric(year), data=bh_t)
+mod.lm
+summary(mod.lm)
+
+mod1<-glm(value ~year, data=hy_t,  na.action = na.exclude, 
+          family = gaussian(link = "identity"))
+
+#+theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) #change angles of labs
+# annotate("text", x=2011, y=3
+bh_t %>% ggplot(aes(x = as.numeric(year),y = log(value+1))) + geom_jitter(shape=16, position=position_jitter(0.1), alpha=0.3)+ geom_smooth(method = "lm")+
+  theme_light()+stat_compare_means(method = "anova", label.y = 7)+ ylab("no. of beehives")+xlab("year")+
+  stat_poly_line() + stat_poly_eq(aes(label = paste(after_stat(eq.label), sep = "*\", \"*")))+xlab("year")
+
+bh_plot<- bh_t %>% ggplot(aes(x = as.numeric(year),y = log(value+1))) + geom_jitter(shape=16, position=position_jitter(0.1), alpha=0.3)+
+  theme_classic()+
+  stat_compare_means(method = "anova", label.y = 7)+ ylab("# Beehives")+xlab("Year")+
+  stat_poly_line() +  scale_x_continuous(breaks = scales::pretty_breaks(n = 5))+
+  geom_label(aes(x = 2018, y = 4.2), hjust = 0, size=5, label = paste("Adj R2 = ",signif(summary(mod.lm)$adj.r.squared, 5)," \nP =",signif(summary(mod.lm)$coef[2,4], 5))) +
+  geom_smooth(method = "lm", color="black", fill="lightgrey")+
+  theme(axis.text.y = element_text(size = 14),
+        axis.title = element_text(size = 14),
+        axis.text.x = element_text(size = 14),
+        panel.background = element_rect(colour = "black", size=1))
+bh_plot
+ggsave(paste0(dirF, "beehive_decline.png"),width=8, height = 10, units="in", dpi=600 ) 
+
+###PLOT WITH PREDICTION LINES
+y2 = log(bh_t$value+1)
+x2 = as.numeric(bh_t$year)
+
+bh_lm <- lm(y2~x2)
+summary(bh_lm)
+
+pred_x2 = c(min(x2),rep(max(x2),2),max(x2)+5)
+pred_lines2 = data.frame(x2=pred_x2,
+                         y2=predict(bh_lm, data.frame(x2=pred_x2)),
+                         obs_Or_Pred=rep(c("Obs","Pred"), each=2))
+
+bh_plot<-ggplot(pred_lines2, aes(x2, y2, colour=obs_Or_Pred, shape=obs_Or_Pred, linetype=obs_Or_Pred)) +
+  geom_jitter(data=data.frame(x2,y2, obs_Or_Pred="Obs"), size=3, position=position_jitter(0.1), alpha=0.3) +
+  geom_line(size=1) +scale_color_manual(values=c('grey50', 'black'))+
+  scale_shape_manual(values=c(16,NA)) +
+  ylab("Beehives per beekeeper")+xlab("Year")+
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 5))+
+  theme_bw()+
+  theme(legend.title=element_blank())+
+  theme(axis.text.y = element_text(size = 14),
+        axis.text.x = element_text(size = 14),
+        axis.title = element_text(size = 14))+
+  geom_label(aes(x = 2009, y = 4.45), hjust = 0, size=4,
+             label = paste("Adj R2 = ",signif(summary(bh_lm)$adj.r.squared, 2)," \nP =",signif(summary(bh_lm)$coef[2,4], 2)), show.legend = FALSE)
+bh_plot
+
+Fig_hy_bh<-ggarrange(hy_plot, bh_plot, labels = c("a", "b"), 
+                     font.label = list(size = 16, color = "black"), ncol = 2, common.legend = TRUE, legend="top")
+annotate_figure(Fig_hy_bh)
+ggsave(paste0(dirF, "hy_bh_decline.png"),width=8, height = 6, units="in", dpi=600 ) 
+
+#+scale_x_continuous(breaks = scales::pretty_breaks(n = 5)), label="p-value: <0.001")
+
+bh_vill_plot<-bh_t %>% ggplot(aes(x = as.numeric(year),y = log(value+1), colour=study_village, fill=study_village)) + geom_point(col="black", alpha=0.3) + 
+  geom_smooth(method = "lm", color="black", fill="lightgrey")+ stat_cor(label.y = c(4.2,4.2,4.2,4.2,4.2,4.2,4.2,4.2,4.2,4.2), col="black")+
+  facet_wrap(~study_village, scales = "fixed") +
+  theme_light()+ ylab("# Beehives")+xlab("Year")+
+  theme(axis.text.y = element_text(size = 14))+ theme(axis.title = element_text(size = 14)) + 
+  theme(axis.text.x = element_text(size = 14, angle=90))+ theme(axis.title = element_text(size = 14))+
+  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
+        strip.background = element_blank(),panel.border = element_rect(colour = "black"))+
+  theme(strip.text.x = element_text(size = 14, color = "black"))+
+  theme(strip.background = element_rect(color="grey0", fill="grey95", linetype="solid"))+
+  theme(legend.position = "none")
+bh_vill_plot
+ggsave(paste0(dirF, "beehive_decline_vill.png"),width=8, height = 10, units="in", dpi=600 ) 
+
+#stat_poly_eq(aes(label = paste(after_stat(eq.label), sep = "*\", \"*")))
+#geom_label(aes(x = 2018, y = 4.2), hjust = 0, size=2, label = paste("Adj R2 = ",signif(summary(mod.lm)$adj.r.squared, 5)," \nP =",signif(summary(mod.lm)$coef[2,4], 5)))
+
+
+
+
+### ADD HISTORIC DATA
 
 #historic beehive change
 bhh<-read.csv("data/hives_historic.csv", sep=";",check.names=FALSE)
---
+++ Rscripts/livelihood_change.R
@@ -0,0 +1,37 @@
+#livlihood change
+ld<-read.csv("data/livelihood_change.csv", sep=";",check.names=FALSE)
+
+perc.ld<-ld %>% select(livelihood_affected) %>% count(livelihood_affected) %>% mutate(freq = n / sum(n)*100)
+  
+ld_causes_perc<-round(apply(ld[,c(4:9)], 2, sum, na.rm=T)/116*100)
+#barplot(ld2)
+
+ld2<-ld[,c(4:9)]
+
+causes_counts<-ld2 %>% summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE))) 
+
+names(causes_counts)<-c("income", "consumption", "beewax", "crop quality decrease", "crop quantity decrease")
+reasons<-c("income", "consumption", "beewax", "crop quality decrease", "crop quantity decrease")
+percentage<-round(causes_counts/116*100)
+
+
+causes2<-rbind(names(causes_counts), causes_counts, as.vector(percentage))
+causes2<-t(causes2)
+colnames(causes2)<-c("reasons", "number", "percentage")
+
+#plot
+ggplot(causes2, aes(x=fct_inorder(reasons), y=as.numeric(percentage))) + 
+  geom_bar(aes(fill = fct_inorder(reasons)), position="dodge", stat = "identity")+
+  scale_fill_grey(start = 0.1, end = .9)+
+  theme_bw()+ coord_flip()+
+  geom_text(aes(label=percentage), vjust= 1, hjust = -0.1, size=4)+
+  theme(text = element_text(size = 16))+
+  ylab("percentage")+ xlab("")+ ggtitle("reasons for the decline")+
+  theme(legend.position = "none")+
+  theme(axis.text.y = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        axis.text.x = element_text(size = 16))
+
+ggsave(paste0(dirF, "causes for decline.png"),width=8, height = 10, units="in", dpi=600 ) 
+
+
